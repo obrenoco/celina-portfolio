52275e9c35a7d763bbc938857463d480e14be641
Breno Romeiro <breno.romeiro@Brenos-MacBook-Pro.local>
2024-02-23 10:45:09 -0300

    add basic layout

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("script works!!");
+function openModal(modalNumber) {
+  var modal = document.getElementById("myModal" + modalNumber);
+  modal.style.display = "block";
+}
+
+function closeModal(modalNumber) {
+  var modal = document.getElementById("myModal" + modalNumber);
+  modal.style.display = "none";
+}
+
+document.getElementById("myBtn1").onclick = function () {
+  openModal(1);
+};
+
+document.getElementById("myBtn2").onclick = function () {
+  openModal(2);
+};
+
+window.onclick = function (event) {
+  var modal1 = document.getElementById("myModal1");
+  var modal2 = document.getElementById("myModal2");
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
